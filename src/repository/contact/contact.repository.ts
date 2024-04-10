9db3abd80e07ae7bc3075e39bd870d188915de45
Rustamxojayev Muhammadxoja <muhammadxojaofficial@gmail.com>
2024-04-11 00:02:18 +0500

    [*] added CRUD operations to contact and deal

--- src/repository/contact/contact.repository.ts
+++ src/repository/contact/contact.repository.ts
@@ -1,33 +1,55 @@
-import {Contact} from "../../domain/contact/contact.domain";
-import {Injectable} from "@nestjs/common";
+import { Contact } from "../../domain/contact/contact.domain";
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class ContactRepository {
     contactRepositoryDB: Contact[];
 
     constructor() {
-        this.contactRepositoryDB = [{
-            _id: "5349b4ddd2781d08c09890f4",
-            name: "John",
-            organization: "google",
-            email: "john@gmail.com",
-            phone: "+998903470144"
-        }]
+        this.contactRepositoryDB = [
+            {
+                _id: "5349b4ddd2781d08c09890f4",
+                name: "John",
+                organization: "google",
+                email: "john@gmail.com",
+                phone: "+998903470144",
+            },
+        ];
     }
 
-    public findAll() {
-
+    public findAll(): Contact[] {
+        return this.contactRepositoryDB;
     }
 
-    public findOne() {
+    public create(contact: Contact): Contact {
+        this.contactRepositoryDB.push(contact);
 
+        return contact;
     }
 
-    public update() {
-
+    public findOne(id: string): Contact {
+        return this.contactRepositoryDB.find((contact: Contact) => {
+            if (contact._id === id) {
+                return contact;
+            }
+        });
     }
 
-    public delete() {
+    public update(contact: Contact): Contact {
+        const contactIndex: number = this.contactRepositoryDB.findIndex(
+            (contactUpdate: Contact) => {
+                if (contactUpdate._id === contact._id) {
+                    return contactUpdate;
+                }
+            },
+        );
 
+        this.contactRepositoryDB[contactIndex] = contact;
+
+        return this.contactRepositoryDB[contactIndex];
+    }
+
+    public delete(id: string): Contact[] {
+        return this.contactRepositoryDB.filter((contact: Contact) => contact._id !== id);
     }
 }
--- src/repository/deal/deal.repository.ts
+++ src/repository/deal/deal.repository.ts
@@ -1,37 +1,57 @@
-import {Deal, DealStatus} from "../../domain/deal/deal.domain";
-import {Injectable} from "@nestjs/common";
+import { Deal, DealStatus } from "../../domain/deal/deal.domain";
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class DealRepository {
     dealRepositoryDB: Deal[];
 
     constructor() {
-        this.dealRepositoryDB = [{
-            _id: "5349b4ddd2781d08c09890f4",
-            first_name: "John",
-            last_name: "Doe",
-            phone: "+998905879038",
-            email: "johndoe@gmail.com",
-            organization: "google",
-            value: 100.00,
-            date: new Date(),
-            status: DealStatus.qualified
-        }]
+        this.dealRepositoryDB = [
+            {
+                _id: "5349b4ddd2781d08c09890f4",
+                first_name: "John",
+                last_name: "Doe",
+                phone: "+998905879038",
+                email: "johndoe@gmail.com",
+                organization: "google",
+                value: 100.0,
+                date: new Date(),
+                status: DealStatus.qualified,
+            },
+        ];
     }
 
-    public findAll() {
-
+    public findAll(): Deal[] {
+        return this.dealRepositoryDB;
     }
 
-    public findOne() {
-
+    public findOne(id: string): Deal {
+        return this.dealRepositoryDB.find((deal: Deal) => {
+            if (deal._id === id) {
+                return deal;
+            }
+        });
     }
 
-    public update() {
+    public create(deal: Deal): Deal {
+        this.dealRepositoryDB.push(deal);
 
+        return deal;
     }
 
-    public delete() {
+    public update(deal: Deal): Deal {
+        const dealIndex: number = this.dealRepositoryDB.findIndex((dealUpdate: Deal) => {
+            if (dealUpdate._id === deal._id) {
+                return dealUpdate;
+            }
+        });
 
+        this.dealRepositoryDB[dealIndex] = deal;
+
+        return this.dealRepositoryDB[dealIndex];
+    }
+
+    public delete(id: string): Deal[] {
+        return this.dealRepositoryDB.filter((deal: Deal) => deal._id !== id);
     }
 }
