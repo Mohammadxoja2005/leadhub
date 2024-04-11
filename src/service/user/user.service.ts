7fd8b35c28ea7e97f97c551e505e48b2e0cea137
Rustamxojayev Muhammadxoja <muhammadxojaofficial@gmail.com>
2024-04-11 22:32:43 +0500

    [*] made service name with impl to further use with interface

--- src/service/user/user.service.ts
+++ src/service/user/user.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { UserRepository } from "../../interfaces";
 import { User } from "../../domain";
+import { repositoryTokens } from "../../common/tokens/repository.tokens";
 
 @Injectable()
-export class UserService {
-    constructor(private readonly userRepository: UserRepository) {}
+export class UserServiceImpl {
+    constructor(@Inject(repositoryTokens.user) private readonly userRepository: UserRepository) {}
 
     public async createUser(user: User): Promise<User> {
         return await this.userRepository.create(user);
