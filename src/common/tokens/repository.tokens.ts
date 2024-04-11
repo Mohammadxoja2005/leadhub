5f7735faabfe51ea33ccd9c6d48dd6b85cc6af10
Rustamxojayev Muhammadxoja <muhammadxojaofficial@gmail.com>
2024-04-11 22:31:30 +0500

    [+] added tokens for service and repository

---
+++ src/common/tokens/repository.tokens.ts
@@ -0,0 +1,6 @@
+export const repositoryTokens = {
+    contact: Symbol.for("ContactRepository"),
+    deal: Symbol.for("DealRepository"),
+    lead: Symbol.for("LeadRepository"),
+    user: Symbol.for("UserRepository"),
+};
---
+++ src/common/tokens/service.tokens.ts
@@ -0,0 +1,6 @@
+export const serviceTokens = {
+    contact: Symbol.for("ContactService"),
+    deal: Symbol.for("DealService"),
+    lead: Symbol.for("LeadService"),
+    user: Symbol.for("UserService"),
+};
