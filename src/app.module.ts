b2826bab77b7f29d0a04560ccce552af77af7cc6
Rustamxojayev Muhammadxoja <muhammadxojaofficial@gmail.com>
2024-04-11 22:33:45 +0500

    [+] added tokens for repository and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,37 @@
-import {Module} from "@nestjs/common";
-import {UserController} from "./controllers";
-import {ContactRepository, DealRepository, UserRepository, LeadRepository} from "./repository";
-import {UserService} from "./service";
+import { Module } from "@nestjs/common";
+import { UserController } from "./controllers";
+import {
+    ContactRepositoryImpl,
+    DealRepositoryImpl,
+    UserRepositoryImpl,
+    LeadRepositoryImpl,
+} from "./repository";
+import { repositoryTokens } from "./common/tokens/repository.tokens";
+import { LeadServiceImpl, UserServiceImpl, DealServiceImpl, ContactServiceImpl } from "./service";
 
 @Module({
     controllers: [UserController],
     providers: [
-        ContactRepository,
-        DealRepository,
-        UserRepository,
-        LeadRepository,
-        UserService
+        {
+            provide: repositoryTokens.deal,
+            useClass: DealRepositoryImpl,
+        },
+        {
+            provide: repositoryTokens.lead,
+            useClass: LeadRepositoryImpl,
+        },
+        {
+            provide: repositoryTokens.user,
+            useClass: UserRepositoryImpl,
+        },
+        {
+            provide: repositoryTokens.contact,
+            useClass: ContactRepositoryImpl,
+        },
+        UserServiceImpl,
+        LeadServiceImpl,
+        DealServiceImpl,
+        ContactServiceImpl,
     ],
 })
-export class AppModule {
-
-}
+export class AppModule {}
