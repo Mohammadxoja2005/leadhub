2ea92ee87fc6e49d8b740df45aa43176d9c4666c
Rustamxojayev Muhammadxoja <muhammadxojaofficial@gmail.com>
2024-04-12 16:28:34 +0500

    [+] added import of services in interface folder

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,8 @@ export * from "./repository/lead/lead.interface";
 export * from "./repository/deal/deal.interface";
 export * from "./repository/contact/contact.interface";
 export * from "./repository/user/user.interface";
+
+export * from "./services/contact/contact.interface";
+export * from "./services/user/user.interface";
+export * from "./services/deal/deal.interface";
+export * from "./services/lead/lead.interface";
