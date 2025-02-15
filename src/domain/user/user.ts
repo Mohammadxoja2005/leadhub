44042bcf8af879ccbc51ec39b041bfea18649622
Rustamxojayev Muhammadxoja <muhammadxojaofficial@gmail.com>
2025-02-15 22:32:39 +0500

    changed user entity

--- src/domain/user/user.ts
+++ src/domain/user/user.ts
@@ -2,9 +2,11 @@ export type User = {
     username: string;
     password: string;
     name: string;
-    phone: string;
     email: string;
-    active: boolean;
+    isActive: boolean;
     role: "admin" | "regular";
     projectId: string;
+    oAuth: {
+        googleId: string;
+    };
 };
