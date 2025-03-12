c0aaeeed123e2ba6ce01412ba91d733f10bcb4bf
Rustamxojayev Muhammadxoja <muhammadxojaofficial@gmail.com>
2025-03-12 21:47:44 +0500

    made some contact create fields optional

--- src/application/api/controllers/contact/create/input.ts
+++ src/application/api/controllers/contact/create/input.ts
@@ -1,12 +1,15 @@
-import { IsString } from "class-validator";
+import { IsOptional, IsString } from "class-validator";
 
 export class Input {
     @IsString()
     public name: string;
+    @IsOptional()
     @IsString()
     organization: string;
+    @IsOptional()
     @IsString()
     email: string;
+    @IsOptional()
     @IsString()
     phone: string;
 }
